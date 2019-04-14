9f2a3f0cf019dd737c8791d8f5fc559caf538d5f
Sanittawan Tan <sanittawan.t@gmail.com>
2019-04-14 11:39:08 -0500

    Updated

--- alt_assignment.py
+++ alt_assignment.py
@@ -9,36 +9,39 @@ from random import shuffle
 
 FIRSTNAME = 'first name'
 LASTNAME = 'last name'
+FIRST = 'first choice'
+SECOND = 'second choice'
+THIRD = 'third choice'
+FOURTH = 'fourth choice'
+FIFTH = 'fifth choice'
 PREF = [FIRST, SECOND, THIRD, FOURTH, FIFTH]
+STUDENT = 'student'
 
 def read_process_spreadsheet(filename):
     ID = 'id'
-    FIRST = 'first choice'
-    SECOND = 'second choice'
-    THIRD = 'third choice'
-    FOURTH = 'fourth choice'
-    FIFTH = 'fifth choice'
-
+    TIME = 'Timestamp'
+    EMAIL = 'email'
     students_df = pd.read_csv(filename)
-    students_df.drop(columns=['Timestamp'], inplace=True)
-    students_df.columns = ['email', FIRSTNAME, LASTNAME, ID, FIRST, 
+    students_df.drop(columns=[TIME], inplace=True)
+    students_df.columns = [EMAIL, FIRSTNAME, LASTNAME, ID, FIRST, 
                            SECOND, THIRD, FOURTH, FIFTH]
     return students_df
 
 def read_country_spreadsheet(filename):
     country_df = pd.read_csv(filename)
     country_df.set_index(['country'], inplace=True)
-    country_df['student'] = 0
+    country_df[STUDENT] = 0
 
     return country_df
 
 def assign():
     allocated_students = set()
     rounds = 5
-    students_df = read_process_spreadsheet('survey.csv')
+    students_df = read_process_spreadsheet('test_students.csv')
     cp_students_df = students_df.copy()
-    assigned_df = read_country_spreadsheet('countries.csv')
+    assigned_df = read_country_spreadsheet('test_countries_small.csv')
 
+    # allocate countries to students based on their preferences
     for i in range(rounds):
         if i > 0:
             shuffle(order) 
@@ -50,25 +53,26 @@ def assign():
         for j in cp_order:
             cur_student = students_df.iloc[j]
             cur_choice = cur_student[PREF[i]]
-            if not cur_choice:
+            if not cur_choice or pd.isna(cur_choice):
                 continue
-            if (assigned_df.loc[cur_choice]['student'] == 0) and\
-                cp_students_df[cp_students_df[PREF[i]] == cur_choice].shape[0] == 1:
+            if (assigned_df.loc[cur_choice][STUDENT] == 0) and\
+                (cp_students_df[cp_students_df[PREF[i]] == cur_choice].shape[0] == 1):
                 assigned_df.loc[cur_choice] = cur_student[LASTNAME]
                 allocated_students.add(cur_student[LASTNAME])
                 order.remove(j)
                 cp_students_df = cp_students_df.drop([j])
     print(cp_students_df)
     print(assigned_df)
+    
     # deal with students who were not allocated any countries so far
     unallocated_students = list(cp_students_df[LASTNAME].values)
-    avail_countries = assigned_df[assigned_df['student'] == 0]
-
-    i = 0
-    for ind, _ in avail_countries.iterrows():
-        assigned_df.loc[ind] = unallocated_students[i]
-        i += 1
+    avail_countries = assigned_df[assigned_df[STUDENT] == 0]
+    for i, student in enumerate(unallocated_students):
+        assigned_df.iloc[i] = student
     print(assigned_df)
+
+if __name__ == '__main__':
+    assign()
 
 
 
