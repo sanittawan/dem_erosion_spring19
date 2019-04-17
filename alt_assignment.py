f18680ac106d6a2623ca8fac38a78ca5a7cff3c5
Sanittawan Tan <sanittawan.t@gmail.com>
2019-04-17 09:23:04 -0500

    Made minor changes

--- alt_assignment.py
+++ alt_assignment.py
@@ -1,7 +1,9 @@
 '''
 Code for assigning students based on their ranked preferences
 
-Author: Sanittawan Tan (Nikki)
+This is a draft. Still need to 
+
+Author: Nikki Tan
 '''
 import pandas as pd
 import numpy as np
@@ -34,14 +36,19 @@ def read_country_spreadsheet(filename):
 
     return country_df
 
-def assign():
+def assign(students_df, country_df):
+	np.random.seed(123)
     allocated_students = set()
     rounds = 5
-    students_df = read_process_spreadsheet('test_students.csv')
     cp_students_df = students_df.copy()
-    assigned_df = read_country_spreadsheet('test_countries_small.csv')
+    assigned_df = country_df
 
     # allocate countries to students based on their preferences
+    # At each round corresponding to ranked preferences,
+    # the idea is to allocate a country where there is only one student who select it
+    # then move on to the second, third, fourth, and fifth rounds
+    # when allocation can no longer be done, the algoritm randomly assign
+    # countries for the rest of the students 
     for i in range(rounds):
         if i > 0:
             shuffle(order) 
@@ -57,7 +64,7 @@ def assign():
                 continue
             if (assigned_df.loc[cur_choice][STUDENT] == 0) and\
                 (cp_students_df[cp_students_df[PREF[i]] == cur_choice].shape[0] == 1):
-                assigned_df.loc[cur_choice] = cur_student[LASTNAME]
+                assigned_df.loc[cur_choice] = cur_student[LASTNAME]    # might use ID instead
                 allocated_students.add(cur_student[LASTNAME])
                 order.remove(j)
                 cp_students_df = cp_students_df.drop([j])
@@ -65,14 +72,18 @@ def assign():
     print(assigned_df)
     
     # deal with students who were not allocated any countries so far
-    unallocated_students = list(cp_students_df[LASTNAME].values)
+    unallocated_students = shuffle(list(cp_students_df[LASTNAME].values))
     avail_countries = assigned_df[assigned_df[STUDENT] == 0]
     for i, student in enumerate(unallocated_students):
         assigned_df.iloc[i] = student
     print(assigned_df)
+    return assigned_df
 
 if __name__ == '__main__':
-    assign()
+    students = read_process_spreadsheet('test_students.csv')
+    countries = read_country_spreadsheet('test_countries_small.csv')
+    assigned = assign(students, countries)
+    # write to csv here
 
 
 
